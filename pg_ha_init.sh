12f0f7a0b1fb08820717fe0f83ea7d53431fe9e6
Tony Pai <tony77794@gmail.com>
2023-08-18 14:06:11 +0800

    Reduce dependencies
    Remove repmgr init script from the service

--- pg_ha_init.sh
+++ pg_ha_init.sh
@@ -8,8 +8,12 @@ while true; do
     output=$(docker-compose exec db2 nc -vz db1 5432  2>&1)
     if [ $? -eq 0 ]; then
         echo "done"
-        docker-compose exec db1 ./repmgr_init.sh
-        docker-compose exec db2 ./repmgr_init.sh
+        docker-compose exec db1 repmgr -f /etc/postgresql/repmgr.conf primary register
+        docker-compose exec db1 repmgr -f /etc/postgresql/repmgr.conf cluster show
+        docker-compose exec -e PGPASSWORD=repmgr db2 repmgr -h db1 -U repmgr -d repmgr -f /etc/postgresql/repmgr.conf standby clone
+        docker-compose exec db2 pg_ctl -D /var/lib/postgresql/data -o '-c config_file=/etc/postgresql/postgresql.conf' start
+        docker-compose exec -e PGPASSWORD=repmgr db2 repmgr -h db1 -U repmgr -d repmgr -f /etc/postgresql/repmgr.conf standby register
+        docker-compose exec -e PGPASSWORD=repmgr db2 repmgr -h db1 -U repmgr -d repmgr -f /etc/postgresql/repmgr.conf cluster show
         break
     fi
     sleep 1
--- repmgr1_init.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-repmgr -f /etc/postgresql/repmgr.conf primary register
-repmgr -f /etc/postgresql/repmgr.conf cluster show
