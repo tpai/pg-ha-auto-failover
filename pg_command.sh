adcb2403fb5447de6226e56a8c2c40daa1f735b9
Tony Pai <tony77794@gmail.com>
2023-08-18 14:06:43 +0800

    Rename failover to switchover and fix script

--- pg_command.sh
+++ pg_command.sh
@@ -10,13 +10,6 @@ if [ ! -f "$PGDATA/pg_hba.conf" ]; then
     tail -f /dev/null
   fi
 else
-  if [ "$1" = "master" ]; then
-    PGPASSWORD=repmgr repmgr -h db2 -U repmgr -d repmgr -f /etc/postgresql/repmgr.conf node rejoin
-    pg_ctl -w -D /var/lib/postgresql/data -o '-c config_file=/etc/postgresql/postgresql.conf' restart
-    repmgrd -f /etc/postgresql/repmgr.conf --daemonize=false --no-pid-file
-  fi
-  if [ "$1" = "slave" ]; then
-    pg_ctl -w -D /var/lib/postgresql/data -o '-c config_file=/etc/postgresql/postgresql.conf' restart
-    repmgrd -f /etc/postgresql/repmgr.conf --daemonize=false --no-pid-file
-  fi
+  pg_ctl -w -D /var/lib/postgresql/data -o '-c config_file=/etc/postgresql/postgresql.conf' restart
+  repmgrd -f /etc/postgresql/repmgr.conf --daemonize=false --no-pid-file
 fi
---
+++ pg_ha_switchover.sh
@@ -0,0 +1,88 @@
+#!/bin/bash
+
+echo "################### Switchover(db1) ###################-"
+
+docker-compose exec db1 repmgr -f /etc/postgresql/repmgr.conf cluster show
+
+echo ">>> create table at db1"
+docker-compose exec db1 psql -c " \
+CREATE TABLE Employees (
+    EmployeeID INT PRIMARY KEY,
+    FirstName VARCHAR(50),
+    LastName VARCHAR(50),
+    Department VARCHAR(50)
+);"
+
+echo ">>> stop db1 instance"
+docker-compose stop db1
+
+echo ">>> sleep 20s until db1 fail"
+sleep 20
+
+docker-compose exec db2 repmgr -f /etc/postgresql/repmgr.conf cluster show
+
+echo ">>>> insert value at db2"
+docker-compose exec db2 psql -c " \
+INSERT INTO Employees (EmployeeID, FirstName, LastName, Department)
+VALUES (1, 'John', 'Doe', 'Sales');"
+
+echo ">>> start db1 instance and rejoin to repmgr"
+docker-compose up db1 -d
+
+while true; do
+    echo "Connecting db1:5432..."
+    output=$(docker-compose exec db1 nc -vz db1 5432  2>&1)
+    if [ $? -eq 0 ]; then
+        echo "done"
+        docker-compose exec db1 pg_ctl -w -D /var/lib/postgresql/data -o '-c config_file=/etc/postgresql/postgresql.conf' stop
+        docker-compose exec -e PGPASSWORD=repmgr db1 repmgr -h db2 -U repmgr -d repmgr -f /etc/postgresql/repmgr.conf node rejoin --force-rewind
+        docker-compose exec db1 pg_ctl -w -D /var/lib/postgresql/data -o '-c config_file=/etc/postgresql/postgresql.conf' restart
+        break
+    fi
+    sleep 1
+done
+
+docker-compose exec db2 repmgr -f /etc/postgresql/repmgr.conf cluster show
+
+echo ">>>> select value from db1"
+docker-compose exec db1 psql -c " \
+SELECT * FROM Employees;"
+
+echo "################### Switchover(db2) ###################-"
+
+docker-compose exec db2 repmgr -f /etc/postgresql/repmgr.conf cluster show
+
+echo ">>> stop db2 instance"
+docker-compose stop db2
+
+echo ">>> sleep 20s until db2 fail"
+sleep 20
+
+docker-compose exec db1 repmgr -f /etc/postgresql/repmgr.conf cluster show
+
+echo ">>>> insert value at db1"
+docker-compose exec db1 psql -c " \
+INSERT INTO Employees (EmployeeID, FirstName, LastName, Department)
+VALUES (2, 'Jane', 'Smith', 'Marketing');"
+
+echo ">>> start db2 instance and rejoin to repmgr"
+docker-compose up db2 -d
+
+while true; do
+    echo "Connecting db2:5432..."
+    output=$(docker-compose exec db2 nc -vz db2 5432  2>&1)
+    if [ $? -eq 0 ]; then
+        echo "done"
+        docker-compose exec db2 pg_ctl -w -D /var/lib/postgresql/data -o '-c config_file=/etc/postgresql/postgresql.conf' stop
+        docker-compose exec -e PGPASSWORD=repmgr db2 repmgr -h db1 -U repmgr -d repmgr -f /etc/postgresql/repmgr.conf node rejoin --force-rewind
+        docker-compose exec db2 pg_ctl -w -D /var/lib/postgresql/data -o '-c config_file=/etc/postgresql/postgresql.conf' restart
+        break
+    fi
+    sleep 1
+done
+
+docker-compose exec db1 repmgr -f /etc/postgresql/repmgr.conf cluster show
+
+echo ">>>> select value from db2"
+docker-compose exec db2 psql -c " \
+SELECT * FROM Employees;"
